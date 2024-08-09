879db59e200491140093bcc648abce035ccc35cb
Meybee-67 <167405254+Meybee-67@users.noreply.github.com>
2024-08-09 10:35:31 +0200

    Update final_code.ino

--- ESP32-code/final_code.ino
+++ ESP32-code/final_code.ino
@@ -9,12 +9,29 @@ const char* ssid = "Access-point";
 const char* password = "123456789";
 
 //Define sensors & variables
+
 #define sensor A0
-#define led 4
+
+// LDR pin connections (photoresistors)
+#define ldrlt A2 //LDR top left - BOTTOM LEFT
+#define ldrrt A3 //LDR top rigt - BOTTOM RIGHT 
+#define ldrld A0 //LDR down left - TOP LEFT
+#define ldrrd  A1 //ldr down rigt - TOP RIGHT
+
+// 180 horizontal & vertical angle MAX
+int servohLimitHigh = 180;
+int servohLimitLow = 180;
+
+//Initialize the two servos
+Servo horizontal;
+Servo vertical;
+int servoh = 90; 
+int servov = 90; 
+int servovLimitHigh = 120;
+int servovLimitLow = 15;
+
+//Initialize temperature sensor
 int An_1;
-int sec=0;
-int currentTime=0;
-bool ledState=LOW;
 void(* resetFunc) (void) = 0;
 
 WebServer server(80);
