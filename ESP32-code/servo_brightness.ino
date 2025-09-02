62fe0e97bd6abf311f64f0c664760a873bdc312c
Meybee-67 <167405254+Meybee-67@users.noreply.github.com>
2025-09-02 13:12:31 +0200

    Update servo_brightness.ino

--- ESP32-code/servo_brightness.ino
+++ ESP32-code/servo_brightness.ino
@@ -54,7 +54,7 @@ void loop()
   int dvert = avt - avd; // check the diffirence of up and down
   int dhoriz = avl - avr;// check the diffirence og left and rigt
 
-  int avg_list = {avt,avd,avl,avr};
+  int avg_list[] = {avt,avd,avl,avr};
   //Calculate the real brightness
   String getBrghtness(int list{}){
   int max_list = list[0];
