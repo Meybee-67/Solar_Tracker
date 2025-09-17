fec357ece38bb7abbc17b38c62e5214d4fa3148c
Meybee-67 <167405254+Meybee-67@users.noreply.github.com>
2025-09-17 14:08:34 +0200

    Update readLRD.ino

--- ESP32-code/readLRD.ino
+++ ESP32-code/readLRD.ino
@@ -9,9 +9,11 @@ int avg_list[4];
 void setup()
 {
   Serial.begin(9600);
+  delay(1000); // Petit délai pour laisser le port série s'initialiser
+  Serial.println("Lecture des photorésistances...");
 }
 
-int readBrightness(int list){
+String readBrightness(int list){
   int min_list = list[0];
   for (byte i = 0; i < sizeof(list); i+=1){
     if(min_list<list[i]){
@@ -21,7 +23,7 @@ int readBrightness(int list){
     float Vout = min_list*(3.3/4095.0);
     int RLDR = 10000.0 * (Vout / (3.3- Vout));
     int brightness = exp(11.72)*pow(RLDR,-0.79);
-    return(brightness);
+    return(String(brightness));
 
 void loop() 
 {
@@ -35,7 +37,8 @@ void loop()
   avg_list[3]=rd;
 
   Serial.print("Luminosité:");
-  Serial.print()
+  Serial.print(" : ");
+  Serial.println(readBrightness(avg_list));
 }
 
 
