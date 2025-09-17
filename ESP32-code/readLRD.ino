c9b8bd38366a29760016c248f651c6d639a01192
Meybee-67 <167405254+Meybee-67@users.noreply.github.com>
2025-09-17 11:34:07 +0200

    Update and rename servo_brightness.ino to readLRD.ino

---
+++ ESP32-code/readLRD.ino
@@ -0,0 +1,23 @@
+//Define pins
+#define ldrlt A2 //LDR top left - BOTTOM LEFT
+#define ldrrt A3 //LDR top rigt - BOTTOM RIGHT 
+#define ldrld A0 //LDR down left - TOP LEFT
+#define ldrrd  A1 //ldr down rigt - TOP RIGHT
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  int lt = analogRead(ldrlt); // top left
+  int rt = analogRead(ldrrt); // top right
+  int ld = analogRead(ldrld); // down left
+  int rd = analogRead(ldrrd); // down rigt
+  
+  
+
+}
+
+
